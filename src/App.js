b69f27e8b0e084547de261a931deaacd4948fe56
depsirhttps <departedsir@gmail.com>
2023-05-17 15:18:29 -0700

    wasd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Permanotes from "./pages/permanotes/Permanotes";
+import { Analytics } from '@vercel/analytics/react';
 
 const App = (props) => {
   return (
-    <Permanotes/>
+    <>
+      <Permanotes/>
+      <Analytics/>
+    </>
   );
 };
 
